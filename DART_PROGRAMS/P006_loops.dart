c9659b7756b8b80fa5b95374ef250f6da3445fa5
jay-java <jayprajapati.tops@gmail.com>
2025-12-20 12:27:55 +0530

    loop patterns

---
+++ DART_PROGRAMS/P006_loops.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  //1.for
+  for(int i=1;i<=5;i++){
+    print(i);
+  }
+  //2.while
+  int j=1;
+  while(j<=5){
+    print(j);
+    j++;
+  }
+  //3.do while
+  int k=1;
+  do{
+    print(k);
+    k++;
+  }while(k<=5);
+  //4.for each
+  
+}
---
+++ DART_PROGRAMS/P007_patterns.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //***
+  //***
+  //***
+  for(int i=1;i<=5;i++){//row
+    for(int j=1;j<=4;j++){//col
+      stdout.write('*');
+    }
+    print('');
+  }
+  for(int i=1;i<=5;i++){//row
+    for(int j=1;j<=i;j++){//col
+      stdout.write('*');
+    }
+    print('');
+  }
+  
+  for(int i=1;i<=5;i++){//row
+    for(int j=1;j<=i;j++){//col
+      stdout.write(j);
+    }
+    print('');
+  }
+  for(int i=1;i<=5;i++){//row
+    for(int j=1;j<=i;j++){//col
+      stdout.write(i);
+    }
+    print('');
+  }
+
+  for(int i=1;i<=5;i++){//row
+    for(int s=4;s>=i;s--){
+      stdout.write(' ');
+    }
+    for(int j=1;j<=i;j++){//col
+      stdout.write('* ');
+    }
+    print('');
+  }
+  
+
+    int counter =1;
+   for(int i=1;i<=6;i++){//row
+    for(int s=5;s>=i;s--){
+      stdout.write(' ');
+    }
+    for(int j=1;j<=i;j++){//col
+      stdout.write('$counter ');
+      counter++;
+    }
+    print('');
+  }
+
+}
